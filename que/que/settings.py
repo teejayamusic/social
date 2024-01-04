8cad0126606f2ea8ce5319cd4fcf928576659009
teejayamusic <142833479+teejayamusic@users.noreply.github.com>
2024-01-04 21:58:34 +0530

    Update settings.py

--- que/que/settings.py
+++ que/que/settings.py
@@ -22,6 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3bk&37*c%c=3(v00&1ssy)#u7!j-4ik18xj#+wz+()-7!&6=jo'
 
+application = get_wsgi_application()
+
 
 ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
 
